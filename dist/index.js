cb03d6defe5c92bb490d9845ec42e28820d8193c
Spring Chiu <fospring@outlook.com>
2023-11-04 11:50:43 +0800

    feat: nested decode value

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,7 @@
 // hide data field: https://stackoverflow.com/questions/4910567/hide-certain-values-in-output-from-json-stringify
 // https://code.tutsplus.com/the-best-way-to-deep-copy-an-object-in-javascript--cms-39655a
 // https://juejin.cn/post/6844904048701751303
+// https://stackoverflow.com/questions/41588068/object-assign-override-nested-property
 function replacer(key, value) {
     if (key == "inner_data_type")
         return undefined;
@@ -64,9 +65,27 @@ console.log("car: ", car);
 console.log("car run: ", car.run(), "show factory" + car.factory.show());
 const serializedCar = JSON.stringify(car, replacer);
 const carObj = JSON.parse(serializedCar);
+const carObj2 = JSON.parse(serializedCar);
 let deserializedCar = carObj;
 deserializedCar.factory = Object.assign(new Factory(), deserializedCar.factory);
 deserializedCar = Object.assign(new Car(), deserializedCar);
 console.log("deserializedCar: ", deserializedCar);
 console.log("deserializedCar run: ", deserializedCar.run(), "show factory" + deserializedCar.factory.show());
+const deserializedCar2 = decodeNested(new Car(), carObj2);
+console.log("deserializedCar2 run: " + deserializedCar2.run(), "deserializedCar2 show factory: ", deserializedCar2.factory.show());
+function decodeNested(instance, obj) {
+    let key;
+    for (key in obj) {
+        // @ts-ignore
+        let value = obj[key];
+        console.log("decodeNested filed, key:  ", key, " value: ", value, "instance[", key, "]: ", instance[key]);
+        if (typeof value == 'object') {
+            console.log("object key: ", key, " value: ", value);
+            // @ts-ignore
+            instance[key] = Object.assign(instance[key], obj[key]);
+        }
+    }
+    instance = Object.assign(instance, deserializedCar);
+    return instance;
+}
 //# sourceMappingURL=index.js.map
