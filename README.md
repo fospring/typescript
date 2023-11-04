edf3ad01063603bdd8e497e6b398004178a9b7df
Spring Chiu <fospring@outlook.com>
2023-11-04 18:31:28 +0800

    feat: use cloneDeep to save Class info

--- README.md
+++ README.md
@@ -18,4 +18,70 @@ student info  Student {
 student sayHello:  My name is: Alice
 student stringify:  {"name":"Alice","age":12}
 studentBack info  { name: 'Alice', age: 12 }
+studentBack sayHello:  My name is: Alice
+car:  Car {
+  name: 'Mercedes-Benz',
+  speed: 240,
+  factory: Factory {
+    name: 'Bremen factory',
+    location: 'Bremen German',
+    director: Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+  }
+}
+car run:  Mercedes-Benz run with speed: 240 show factoryfactory name: Bremen factory location: Bremen German
+deserializedCar:  Car {
+  name: 'Mercedes-Benz',
+  speed: 240,
+  factory: Factory {
+    name: 'Bremen factory',
+    location: 'Bremen German',
+    director: { name: 'Thomas Müller', age: 40, sex: 'male' }
+  }
+}
+deserializedCar run:  Mercedes-Benz run with speed: 240 show factoryfactory name: Bremen factory location: Bremen German
+decodeNested filed, key:   name  value:  Mercedes-Benz instance[ name ]:  
+decodeNested filed, key:   speed  value:  240 instance[ speed ]:  0
+decodeNested filed, key:   factory  value:  {
+  name: 'Bremen factory',
+  location: 'Bremen German',
+  director: { name: 'Thomas Müller', age: 40, sex: 'male' }
+} instance[ factory ]:  Factory {
+  name: '',
+  location: '',
+  director: Person { name: '', age: 0, sex: 'male' }
+}
+object fields, object key:  factory  value:  {
+  name: 'Bremen factory',
+  location: 'Bremen German',
+  director: { name: 'Thomas Müller', age: 40, sex: 'male' }
+}
+decodeNested filed, key:   name  value:  Bremen factory instance[ name ]:  
+decodeNested filed, key:   location  value:  Bremen German instance[ location ]:  
+decodeNested filed, key:   director  value:  { name: 'Thomas Müller', age: 40, sex: 'male' } instance[ director ]:  Person { name: '', age: 0, sex: 'male' }
+object fields, object key:  director  value:  { name: 'Thomas Müller', age: 40, sex: 'male' }
+decodeNested filed, key:   name  value:  Thomas Müller instance[ name ]:  
+decodeNested filed, key:   age  value:  40 instance[ age ]:  0
+decodeNested filed, key:   sex  value:  male instance[ sex ]:  male
+current instance:  Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+instance[key] value Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+current instance:  Factory {
+  name: 'Bremen factory',
+  location: 'Bremen German',
+  director: Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+}
+instance[key] value Factory {
+  name: 'Bremen factory',
+  location: 'Bremen German',
+  director: Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+}
+current instance:  Car {
+  name: 'Mercedes-Benz',
+  speed: 240,
+  factory: Factory {
+    name: 'Bremen factory',
+    location: 'Bremen German',
+    director: Person { name: 'Thomas Müller', age: 40, sex: 'male' }
+  }
+}
+deserializedCar2 run: Mercedes-Benz run with speed: 240 deserializedCar2 show factory:  factory name: Bremen factory location: Bremen German factor director info:  name is: Thomas Müller age: 40 sex: male
 ```
--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // hide data field: https://stackoverflow.com/questions/4910567/hide-certain-values-in-output-from-json-stringify
 // https://code.tutsplus.com/the-best-way-to-deep-copy-an-object-in-javascript--cms-39655a
 // https://juejin.cn/post/6844904048701751303
 // https://stackoverflow.com/questions/41588068/object-assign-override-nested-property
+const lodash = require("lodash");
 function replacer(key, value) {
     if (key == "inner_data_type")
         return undefined;
@@ -32,9 +34,20 @@ class Factory {
     constructor() {
         this.name = "";
         this.location = "";
+        this.director = new Person();
     }
     show() {
         return "factory name: " + this.name + " location: " + this.location;
+    }
+}
+class Person {
+    constructor() {
+        this.name = "";
+        this.age = 0;
+        this.sex = "male";
+    }
+    showInfo() {
+        return "name is: " + this.name + " age: " + this.age + " sex: " + this.sex;
     }
 }
 class Car {
@@ -61,6 +74,9 @@ car.name = "Mercedes-Benz";
 car.speed = 240;
 car.factory.name = "Bremen factory";
 car.factory.location = "Bremen German";
+car.factory.director.name = "Thomas Müller";
+car.factory.director.age = 40;
+car.factory.director.sex = "male";
 console.log("car: ", car);
 console.log("car run: ", car.run(), "show factory" + car.factory.show());
 const serializedCar = JSON.stringify(car, replacer);
@@ -72,7 +88,7 @@ deserializedCar = Object.assign(new Car(), deserializedCar);
 console.log("deserializedCar: ", deserializedCar);
 console.log("deserializedCar run: ", deserializedCar.run(), "show factory" + deserializedCar.factory.show());
 const deserializedCar2 = decodeNested(new Car(), carObj2);
-console.log("deserializedCar2 run: " + deserializedCar2.run(), "deserializedCar2 show factory: ", deserializedCar2.factory.show());
+console.log("deserializedCar2 run: " + deserializedCar2.run(), "deserializedCar2 show factory: ", deserializedCar2.factory.show(), "factor director info: ", deserializedCar2.factory.director.showInfo());
 function decodeNested(instance, obj) {
     let key;
     for (key in obj) {
@@ -80,12 +96,20 @@ function decodeNested(instance, obj) {
         let value = obj[key];
         console.log("decodeNested filed, key:  ", key, " value: ", value, "instance[", key, "]: ", instance[key]);
         if (typeof value == 'object') {
-            console.log("object key: ", key, " value: ", value);
+            console.log("object fields, object key: ", key, " value: ", value);
             // @ts-ignore
-            instance[key] = Object.assign(instance[key], obj[key]);
+            instance[key] = decodeNested(instance[key], obj[key]);
+            console.log("instance[key] value", instance[key]);
         }
     }
-    instance = Object.assign(instance, deserializedCar);
+    const instance_tmp = lodash.cloneDeep(instance);
+    instance = Object.assign(instance, obj);
+    for (key in obj) {
+        if (typeof instance[key] == 'object') {
+            instance[key] = instance_tmp[key];
+        }
+    }
+    console.log("current instance: ", instance);
     return instance;
 }
 //# sourceMappingURL=index.js.map
